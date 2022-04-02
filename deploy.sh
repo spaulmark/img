6b53a31b9e229d579565f833039470e335356776
Spencer <fdoou1@gmail.com>
2022-04-02 19:49:13 -0400

    Update docs to be more contributor friendly

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Download images --- Note that if you cloned this repo, you can't run this file because it's private.
-python ./scraper.py
+if [ -f ./scraper.py ]; then
+    python ./scraper.py
+fi
 
 # Create root dir.json
 
